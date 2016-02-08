f3cf10ef4da4eb72c146b5616b9fe352d2bb6681
Jan Inge Husby <jih@itema.no>
2016-02-08 13:03:08 +0100

    Initial Commit

---
+++ QuizTests/QuizTests.swift
@@ -0,0 +1,36 @@
+//
+//  QuizTests.swift
+//  QuizTests
+//
+//  Created by Jan Inge Husby on 08/02/16.
+//  Copyright © 2016 Jan Inge Husby. All rights reserved.
+//
+
+import XCTest
+@testable import Quiz
+
+class QuizTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
